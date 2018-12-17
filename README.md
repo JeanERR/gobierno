d1ec97206a177641f93705fb5fe7bb0dd1ba4b57
Fish <jean_10_15@outlook.es>
2018-12-17 16:47:35 -0500

    falta pasarela

---
+++ README.md
@@ -0,0 +1,4 @@
+#Clone 
+
+-Install npm install -g @angular/cli
+-go to folder and run "npm install for node_modules
---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBiBu05AL6oMHrXBQ1XqMxHKAyFo5A8EEc",
+  authDomain: "e-commerce-e703f.firebaseapp.com",
+  databaseURL: "https://e-commerce-e703f.firebaseio.com",
+  projectId: "e-commerce-e703f",
+  storageBucket: "e-commerce-e703f.appspot.com",
+  messagingSenderId: "946438494733"
+};
